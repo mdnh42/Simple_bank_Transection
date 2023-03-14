c311cd8004aaf77f4e6920049283428c25d52a7e
hiz <hasaninternetzone@gmail.com>
2023-03-14 11:27:32 +0600

    Completed

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ document.getElementById('btn-submit').addEventListener('click', function()
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if(email === 'sontan@baap.com' && password == 'secret')
+    if(email === 'hasan@bank.com' && password == 'IhateMoney')
     {
         window.location.href = 'bank.html';
     }
--- js/widthdraw.js
+++ js/widthdraw.js
@@ -2,8 +2,28 @@ document.getElementById('btn-widthdraw').addEventListener('click', function(){
     const widthdraw = document.getElementById('widthdrawamout');
     const widthdrawAmount = widthdraw.value; 
     
-    const widthdrawPlacement = document.getElementById('deposite_placement');
-    widthdrawPlacement.innerText; 
+    
+    const widthdrawPlacement = document.getElementById('widthdraw-placement');
+    const widthdrawvalue = widthdrawPlacement.innerText; 
+    const previouswiddrawToal=  parseInt(widthdrawvalue);
 
+    const currentWidthdrawTotal = parseInt (previouswiddrawToal) + parseInt(widthdrawAmount) ;
+    widthdrawPlacement.innerText = currentWidthdrawTotal;
+ 
+    console.log(currentWidthdrawTotal);
+
+    const totalBalance = document.getElementById('total-balance');
+    const previousBal = totalBalance.innerText;
+    const previousBalInt = parseInt(previousBal);
+    
+    if (previousBalInt<currentWidthdrawTotal) {
+        alert("You Cant");
+    }
+    else{
+        const newbalcen = previousBalInt -currentWidthdrawTotal;
+        console.log(previousBalInt);
+        totalBalance.innerText = newbalcen;
+    }
+   
    
 })
